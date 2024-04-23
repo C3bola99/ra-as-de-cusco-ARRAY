c94a4881997183535a2f718d0603ec0d4ae77785
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-23 14:54:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var raçaDeCusco = ['Salsichinha','Yorkshire','Shitzu','Poodle','Rottwailer']
+let frase = prompt("insira um número de 0 a 4")
+frase = Number(frase)
+console.log("a raça escolhida foi:", raçaDeCusco[frase] )
