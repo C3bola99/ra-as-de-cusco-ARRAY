d82636da915979da8ea28f272a543bc6c24103d7
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-23 16:41:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-var raçaDeCusco = ['Salsichinha','Yorkshire','Shitzu','Poodle','Rottwailer']
-let frase = prompt("insira um número de 0 a 4")
-frase = Number(frase)
-console.log("a raça escolhida foi:", raçaDeCusco[frase] )
+//let raçaDeCusco = ['Salsichinha','Yorkshire','Shitzu','Poodle','Rottwailer']
+//let frase = prompt("insira um número de 0 a 4")
+//frase = Number(frase)
+//console.log("a raça escolhida foi:", raçaDeCusco[frase] )
+
+
+//let meuArray = [1, 2, 3, 4, 5, 6];
+//let tamanhoOriginal = meuArray.length;
+//console.log("Tamanho original do array:", tamanhoOriginal);
+//meuArray.push(7);
+//meuArray.splice(3, 2);
+//let novoTamanho = meuArray.length;
+//console.log("Novo tamanho do array:", novoTamanho);
+//console.log(meuArray);
+
+//Exercícios de interpretação
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+//a- nao retorna nada
+//b- retorna nulo
+//c- retorna 11(.LENGHT)
+//d- retorna 3
+//e- retorna uma array(11)
+//f- retorna 9
+
+//Exercícios de interpretação 2
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Qual será o valor impresso no console se a entrada do usuário for:  "Subi num ônibus em Marrocos"?
+
+//o valor impresso será de: SUBI NUM ONIBUS EM MIRROCOS (LENGHT(27))
+
+//Exercícios de escrita de código
+//exercício 1
+
